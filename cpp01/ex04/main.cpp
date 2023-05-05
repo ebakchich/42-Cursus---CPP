0204f8d2a69e7c7bcc3f1bbf6e016d2d1c910209
El houssaine Bakchich <ebakchic@e2r13p11.1337.ma>
2023-05-05 13:43:06 +0100

    cp01 ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -2,17 +2,29 @@
 #include <fstream>
 #include <string>
 
+void replaceAll(std::string& str, const std::string& s1, const std::string& s2) {
+    size_t pos = 0;
+    while ((pos = str.find(s1, pos)) != std::string::npos) {
+        str.replace(pos, s1.length(), s2);
+        pos += s2.length();
+    }
+}
+
 int main(int ac, char **av)
 {
     (void)ac;
+
     std::string     line;
     std::ifstream   input_file(av[1]);
+    std::ofstream   output_file("filename.txt");
 
-    while (std::getline(input_file, line)) {
-        std::cout << line << std::endl;
+    while (std::getline(input_file, line))
+    {
+        replaceAll(line, av[2], av[3]);
+        output_file << line << std::endl;
     }
-
     input_file.close();
+    output_file.close();
 
     return 0;
 }
